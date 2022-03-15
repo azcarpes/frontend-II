ca75e50e6af974c74b54ab71ff1b96dc05e9ceef
azcarpes <azcarpes@hotmail.com>
2022-03-14 22:57:23 -0300

    inicio da mesa, aula 8-eventos

---
+++ Aula 8 - Eventos/script.js
@@ -0,0 +1,18 @@
+//Arivaldo Lopes Camargo
+//Iraildes Nogueira Vieira
+//Jonathan Alcantara Passoli
+//Gabriel Cesar Pereira
+//Aline Zanini Carpes
+//Alisson Campolina
+
+
+
+let naoRecarregar = document.getElementById("idBotaoSalvar");
+
+naoRecarregar.addEventListener ('click', event => event.preventDefault());
+
+
+
+function alerta() {
+    alert("Olá!")
+} 
